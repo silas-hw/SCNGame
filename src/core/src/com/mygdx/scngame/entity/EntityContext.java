4ece90767b91009d80e839defe33e5c85395337e
Silas HW <silas.hayes.williams@gmail.com>
2024-06-28 15:32:30 +0100

    Move TODO comment outside of javadoc

--- src/core/src/com/mygdx/scngame/entity/EntityContext.java
+++ src/core/src/com/mygdx/scngame/entity/EntityContext.java
@@ -3,10 +3,9 @@ package com.mygdx.scngame.entity;
 /**
  * The context for which a {@link Entity entity} may reside within. It is the point of contact
  * between an Entity and external state.
- *
- * TODO: add event handling/message bus so entities can communicate
  */
 public interface EntityContext {
+    // TODO: add event handling/message bus so entities can communicate
     void addEntity(Entity entity);
     void removeEntity(Entity entity);
     void clearEntities();
