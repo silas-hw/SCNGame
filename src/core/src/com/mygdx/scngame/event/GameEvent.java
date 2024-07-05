4010f469cc9749a525e6d6c38044e609ad6887b5
Silas HW <silas.hayes.williams@gmail.com>
2024-07-05 17:28:35 +0100

    Update GameEvent

--- src/core/src/com/mygdx/scngame/event/GameEvent.java
+++ src/core/src/com/mygdx/scngame/event/GameEvent.java
@@ -5,26 +5,12 @@ public class GameEvent {
     /** The source object which initiated the event */
     private final Object source;
 
-    public enum EventType {
-        ENTITY_EVENT,
-        PLAYER_EVENT,
-        DIALOG_EVENT,
-        ENV_EVENT,
-        DEFAULT
-    }
-
-    protected final EventType type = EventType.DEFAULT;
-
     public GameEvent(Object source) {this.source = source;}
 
     public Object getSource() {return this.source;}
 
-    public EventType getType() {
-        return this.type;
-    }
-
     @Override
     public String toString() {
-        return "GameEvent<Source: " + source.toString() + ", Type: " + type + ">";
+        return "GameEvent<Source: " + source.toString() + ">";
     }
 }
