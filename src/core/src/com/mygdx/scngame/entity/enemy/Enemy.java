184691fdc2df0600d328d2ae7a4ee03194f421b9
Silas HW <silas.hayes.williams@gmail.com>
2024-09-05 23:17:52 +0100

    Add notes

--- src/core/src/com/mygdx/scngame/entity/enemy/Enemy.java
+++ src/core/src/com/mygdx/scngame/entity/enemy/Enemy.java
@@ -9,6 +9,15 @@ Current Plan/Idea:
     - attacks may be a bit more complicated. Some attack we would want as their own state, such as sword
       attacks or projectiles. However, I also plan on having a simple 'bash' attack where the enemy constantly
       has a damage box around them
+
+    - simplest solution for attacks would be them being their own state, and having attacks triggered by
+      its own 'attack radius' similar to the detection radius for going hostile.
+
+      Perhaps in the base hostile state there could be an 'attack' timer, with 'attack speed' being set in
+      the EnemyType as well. If the player is in the 'attack radius' they attack, and continue attacking according to
+      the speed. Perhaps we could have some more complicated directional logic so some enemies only attack if facing
+      the player? Or that could be overkill. Enemies that might want to *always* be attacking, such as a ranged enemy
+      that shoots in every direction at a constant rate, could just be set to have a very large attack radius.
  */
 
 import com.badlogic.gdx.graphics.g2d.Animation;
