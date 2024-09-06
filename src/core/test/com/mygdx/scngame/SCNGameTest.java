072f97832f32f1c8552b7bd75b9a74d2ca4b50f5
Silas HW <silas.hayes.williams@gmail.com>
2024-09-06 18:31:54 +0100

    Add failing test to check CI status checks

---
+++ src/core/test/com/mygdx/scngame/SCNGameTest.java
@@ -0,0 +1,13 @@
+package com.mygdx.scngame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SCNGameTest {
+
+    @Test
+    public void fails() {
+        fail();
+    }
+}
